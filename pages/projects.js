ab825ea921166c5279b9d9e7762b1c2e092b6c3f
Ethan Zimbelman <itszimbo@gmail.com>
2020-07-19 17:41:51 -0500

    cal poly cat program card updated

--- pages/projects.js
+++ pages/projects.js
@@ -13,14 +13,14 @@ const projectCardData = [
         pictures: ["./images/ecoslo-project/home.png", "./images/ecoslo-project/login.png", "./images/ecoslo-project/add.png", "./images/ecoslo-project/view.png", "./images/ecoslo-project/update.png", "./images/ecoslo-project/alter.png"]
     },
     {
-        projectName: 'Cal Poly Cat Program Cat Profiles',
+        projectName: 'Cal Poly Cat Program Cat Directory',
         cardTilePicture: "./images/cpcp-project/cpcp-card-tile.jpg",
         productDescription:
-        "We created an interface to a new SQL database storing beach cleanup data, which was previously stored in spreadsheets. Our product allows ECOSLO to fully manage and utilize years worth of data easily to spread public awareness and gain funding.",
-        techStackDescription: "React.js Node.js Express PostgreSQL Heroku",
-        teamMembersDescription: "Elena Fowler, Anna Reid, Ben Glossner, Ethan Abrams, Comal Virdi, Reilly Salkowski",
-        githubURL: "https://github.com/hack4impact-calpoly/EcosloDataApp",
-        pictures: ["./images/ecoslo-project/home.png", "./images/ecoslo-project/login.png"]
+        "We developed a web and mobile app to allow the Cal Poly Cat Program to easily keep track of their cats, documents, and upcoming events, which were previously stored in an excel sheet. Now, volunteers and coordinators can spend more time with the cats instead of on paperwork.",
+        techStackDescription: "Django, Python, PostgreSQL, Swift, AWS, Heroku",
+        teamMembersDescription: "Ethan Zimbelman, Finlay Piroth, Jillian Quinn, Jay Sung, Evan Witulski, Sydney Nguyen",
+        githubURL: "https://github.com/hack4impact-calpoly/calpoly-cat-program",
+        pictures: ["./images/cpcp-project/home.png", "./images/cpcp-project/cat_profile.png", "./images/cpcp-project/events.png"]
     },
     {
         projectName: 'SLO Botanical Gardens Volunteer Hour Tracker',
