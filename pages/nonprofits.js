3169692cedba717acbd86d2c0fca74f815ba3580
Ethan Zimbelman <itszimbo@gmail.com>
2020-07-23 18:05:09 -0500

    nonprofit content update

--- pages/nonprofits.js
+++ pages/nonprofits.js
@@ -23,18 +23,19 @@ class Nonprofits extends React.Component {
             </div>
             
             <Timeline title="Application Process">
-               <Event title="Initial Contact" date="Early Summer">
+               <Event title="Initial Contact" date="Early July">
                   Reach out to our team to get started in our application process!
                   We are interested in hearing about your organization and potential
                   projects you may have in mind.
                </Event>
-               <Event title="Project Information" date="Late Summer">
+               <Event title="Project Information" date="Late July / Early August">
                    We will reach out to you to get some specifics about the project
                    you have in mind. Such questions include the use case, potential
-                   users, and other important things.
+                   users, and additional factors that relate to your product.
                </Event>
                <Event title="Director Consoltation" date="September">
-                   The board of directors will consider the feasibility of your project for our teams.
+                   The board of directors will consider the feasibility of your project
+                   and how well it suits our teams and timeline.
                </Event>
                <Event title="Response" date="Late September">
                    We will let you know if we have choosen to take on your project.
@@ -45,20 +46,37 @@ class Nonprofits extends React.Component {
             </Timeline>
 
             <FAQ>
-               <question
-                  ask="What does the project timeline look like?"
-               >
-                  Most development begins at the start of the new year and continues
-                  until around mid-April. Throughout all of this, we remain in constant
-                  contact with you to ensure we are keeping our project in the right
-                  direction.
+               <question ask="What types of projects do you undertake?">
+                  Most of the projects we undertake are web applications. Common examples
+                  of problems we tackle include volunteer tracking forms, member directories,
+                  and data tracking softwares. Check out our <Link href="/projects"><a>projects
+                  page</a></Link> to see some of our previous work, and feel free to contact
+                  us with any project ideas!
                </question>
-               <question
-                  ask="How much does this cost?"
-               >
-                  These projects cost you nothing! Hack4Impact runs on the idea of doing
-                  good because it's right. We're also students, so you are paying us in
-                  real world experience.
+               <question ask="How much does this cost?">
+                  These projects are free or low cost ($5- $15 / month) for the website
+                  and hosting expenses. We are building software to better help you
+                  serve your community, and we know how tight money can be so we try to
+                  keep these expenses as low as possible.
+               </question>
+               <question ask="What does the project timeline look like?">
+                  Most development begins at the start of the new year and continues
+                  until around mid-April. Throughout all of this, we will remain in
+                  constant contact with you to ensure that we are building an application
+                  that you will want to use.
+               </question>
+               <question ask="How much involvement is expected from nonprofits?">
+                  We strongly believe in continuous and transparent communication to ensure
+                  that your project is something that you will be happy with. This means we
+                  will be asking for constant feedback throughout the development cycle,
+                  including check-ins and surveys.
+               </question>
+               <question ask="How does long term maintence work?">
+                  Once we hand the product off to you around mid-April, we want to give you time
+                  to experiment with it and find anything that needs to be changed. Once we end
+                  our school year in mid-June, we will no longer be able to make additions as easily
+                  due to limited resources on our end. If you encounter bugs on the site, we will
+                  attempt to correct these issues, but cannot guarantee that we will be able to.
                </question>
             </FAQ>
          </>
