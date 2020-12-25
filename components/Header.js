fff66694df9ac8bdeddcc85f8f626a4de6a23b6c
Ethan Zimbelman <itszimbo@gmail.com>
2020-12-25 11:45:15 -0600

    transition to Link for Next routing

--- components/Header.js
+++ components/Header.js
@@ -1,48 +1,38 @@
-import {Nav, Navbar} from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import styles from "../styles/Header.module.css";
 
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const ActiveLink = React.forwardRef(({ children, href }, ref) => {
-   const router = useRouter()
-   const linkClass = router.pathname === href ? styles.activeLink + " nav-link" : "nav-link";
+const Brand = () => (
+  <Link href='/' passHref>
+    <Navbar.Brand>
+      <img
+        src="/images/H4I-logo.png"
+        height="27"
+        alt="Hack4Impact Logo"
+        className="d-inline-block align-text-bottom"
+      />{' '}
+      <span className={styles.navBrand}>cal poly</span>
+    </Navbar.Brand>
+  </Link>
+);
 
-   return (
-      <a href={href} className={linkClass}>
-         {children}
-      </a>
-   )
-});
+const Header = () => (
+  <Navbar bg="light" expand="md" fixed="top">
+    <Brand />
 
-class Header extends React.Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="md" fixed="top" onSelect="test()">
-        <Navbar.Brand href="/">
-          <img
-            src="/images/H4I-logo.png"
-            height="27"
-            alt="Hack4Impact Logo"
-            className="d-inline-block align-text-bottom"
-          />{' '}
-          <span className={styles.navBrand}>cal poly</span>
-        </Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className={styles.navbarToggler} />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Link href="/about-us" passHref><ActiveLink>About Us</ActiveLink></Link>
-            <Link href="/projects" passHref><ActiveLink>Projects</ActiveLink></Link>
-            <Link href="/students" passHref><ActiveLink>Students</ActiveLink></Link>
-            <Link href="/nonprofits" passHref><ActiveLink>Nonprofits</ActiveLink></Link>
-            <Link href="/contact-us" passHref><ActiveLink>Contact Us</ActiveLink></Link>
-            <a target="_blank" href="https://securelb.imodules.com/s/699/bp19/interior.aspx?sid=699&gid=1&sitebuilder=1&pgid=961&cid=2272&bledit=1&dids=623"><ActiveLink>Donate</ActiveLink></a>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
-}
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" className={styles.navbarToggler} />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+        <Link href="/about-us" passHref><Nav.Link>About Us</Nav.Link></Link>
+        <Link href="/projects" passHref><Nav.Link>Projects</Nav.Link></Link>
+        <Link href="/students" passHref><Nav.Link>Students</Nav.Link></Link>
+        <Link href="/nonprofits" passHref><Nav.Link>Nonprofits</Nav.Link></Link>
+        <Link href="/contact-us" passHref><Nav.Link>Contact Us</Nav.Link></Link>
+        <a target="_blank" href="https://securelb.imodules.com/s/699/bp19/interior.aspx?sid=699&gid=1&sitebuilder=1&pgid=961&cid=2272&bledit=1&dids=623"><Nav.Link>Donate</Nav.Link></a>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export default Header;
