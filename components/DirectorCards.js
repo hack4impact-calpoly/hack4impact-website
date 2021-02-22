44483ee9c930e4316fc9c813978b9ab93a1bec87
Ethan Zimbelman <itszimbo@gmail.com>
2021-02-21 17:31:09 -0800

    tweak: cleaned code up

--- components/DirectorCards.js
+++ components/DirectorCards.js
@@ -2,53 +2,44 @@ import { Card } from "react-bootstrap";
 import { SocialIcon } from "react-social-icons";
 import styles from "../styles/directorCards.module.css";
 
-class DirectorCards extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <Card
-          className="bg-white text-black"
-          style={{
-            // overflow: "auto",
-            width: "100%",
-            height: "100%",
-            // borderRadius: "50%",
-            textAlign: "center",
-            alignItems: "center",
-            marginBottom: "0.4rem"
-          }}
-        >
-          <Card.Img
-            style={{
-              objectFit: "cover",
-              width: "100%",
-              height: "200px",
-            }}
-            src={this.props.backgroundImage}
-          />
-          <Card.Text style={{ marginTop:".4rem",marginBottom: ".2rem" }}>
-            <b>{this.props.name}</b>
-          </Card.Text>
-          <Card.Text style={{ marginBottom: ".2rem" }}>
-            {this.props.position}
-          </Card.Text>
-          {this.props.linkedIn ? (
-            <SocialIcon
-              style={{ height: 30, width: 30 }}
-              target="_blank"
-              url={this.props.linkedIn}
-            />
-          ) : (
-            " "
-          )}
-        </Card>
-      </>
-    );
-  }
-}
+const DirectorCards = props => (
+  <>
+    <Card
+      className="bg-white text-black"
+      style={{
+        // overflow: "auto",
+        height: "100%",
+        width: "auto",
+        // borderRadius: "50%",
+        textAlign: "center",
+        alignItems: "center",
+        marginBottom: "0.4rem"
+      }}
+    >
+      <Card.Img
+        style={{
+          objectFit: "cover",
+          height: "200px",
+        }}
+        src={props.backgroundImage}
+      />
+      <Card.Text style={{ marginTop:".4rem",marginBottom: ".2rem" }}>
+        <b>{props.name}</b>
+      </Card.Text>
+      <Card.Text style={{ marginBottom: ".2rem" }}>
+        {props.position}
+      </Card.Text>
+      {props.linkedIn ? (
+        <SocialIcon
+          style={{ height: 30, width: 30 }}
+          target="_blank"
+          url={props.linkedIn}
+        />
+      ) : (
+        " "
+      )}
+    </Card>
+  </>
+);
 
 export default DirectorCards;
