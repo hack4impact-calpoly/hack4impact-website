c68f089e2f98ba37e04ef324b75218d39fa6bc4e
Ethan Zimbelman <itszimbo@gmail.com>
2021-08-20 23:51:28 -0700

    default tag styles created

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,65 +5,26 @@ import getProjectsByYear from '../utils/projects.tsx';
 import styles from '../styles/Home.module.css';
 
 const Home = () => (
-  <div className={styles.container}>
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to
-        {' '}
-        <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+  <>
+    <main>
+      <h1>Hack4Impact Cal Poly</h1>
+      <p>redesign time!</p>
 
-      <p className={styles.description}>
-        Get started by editing
-        {' '}
-        <code className={styles.code}>pages/index.js</code>
-      </p>
-
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h2>Documentation &rarr;</h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h2>Learn &rarr;</h2>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className={styles.card}
-        >
-          <h2>Examples &rarr;</h2>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h2>Deploy &rarr;</h2>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h2>style list</h2>
+      <hr />
+      <h1>heading 1</h1>
+      <h2>heading 2</h2>
+      <h3>heading 3</h3>
+      <h4>heading 4</h4>
+      <h5>heading 5</h5>
+      <a href="/">link &rarr;</a>
+      <p>a paragraph that has text. fancy</p>
     </main>
 
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by
-        {' '}
-        <span className={styles.logo}>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </span>
-      </a>
+      <a href="/">home</a>
     </footer>
-  </div>
+  </>
 );
 
 export async function getStaticProps() {
