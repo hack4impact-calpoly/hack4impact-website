be245d91a0482310f73505c82ac0a6d0fc06fbf0
JillianQuinn <jillian@jillianq.com>
2021-08-27 22:20:54 -0700

    eslint fixes

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,16 +1,21 @@
-import * as React from "react";
-import styles from "../styles/InfoCard.module.css";
+import * as React from 'react';
+import styles from '../styles/InfoCard.module.css';
 
 interface InfoCardProps {
     Title: string;
-    Image?: string;
+    Image: string;
     Body: string;
 }
 
-export const InfoCard: React.FC<InfoCardProps> = ({ Title, Image, Body }) => (
+const InfoCard: React.FC<InfoCardProps> = (props: InfoCardProps): React.ReactElement => {
+  const { Title, Image, Body } = props;
+  return (
     <div>
-        {Image && <img src={Image} className={styles.ICImage} />}
-        <div className={styles.ICTitle}>{Title}</div>
-        <p className={styles.ICBody}>{Body}</p>
+      {Image && <img src={Image} alt="InfoCard" className={styles.ICImage} />}
+      <div className={styles.ICTitle}>{Title}</div>
+      <p className={styles.ICBody}>{Body}</p>
     </div>
-);
+  );
+};
+
+export default InfoCard;
