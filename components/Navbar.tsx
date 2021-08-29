709cf5b00cdf2f6ac78192d652d0333697498a47
Anna Reid <41653027+AnnaReid22@users.noreply.github.com>
2021-08-28 22:06:00 -0700

    H4I image & style updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,35 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
+import config from '../config';
 
 const Navbar = () => (
   <nav className={styles.nav}>
     <div className={styles.logo}>
+      <div className={styles.imageBlah}>
+        <Image src={config.assets.logo_color} alt="hack4impact logo, with color" />
+      </div>
       <Link href="/">
-        <a href="/">Hack4Impact Cal Poly</a>
+        <a>Hack4Impact Cal Poly</a>
       </Link>
     </div>
     <div className={styles.links}>
       <div>
         <Link href="/about">
-          <a href="/about" className={styles.link}>About</a>
+          <a className={styles.link}>About</a>
         </Link>
         <Link href="/projects">
-          <a href="/projects" className={styles.link}>Projects</a>
+          <a className={styles.link}>Projects</a>
         </Link>
         <Link href="/students">
-          <a href="/students" className={styles.link}>Students</a>
+          <a className={styles.link}>Students</a>
         </Link>
         <Link href="/nonprofits">
-          <a href="/nonprofits" className={styles.link}>Nonprofits</a>
+          <a className={styles.link}>Nonprofits</a>
         </Link>
         <Link href="https://securelb.imodules.com/s/699/bp19/interior.aspx?sid=699&gid=1&sitebuilder=1&pgid=961&cid=2272&bledit=1&dids=623">
-          <a
-            href="https://securelb.imodules.com/s/699/bp19/interior.aspx?sid=699&gid=1&sitebuilder=1&pgid=961&cid=2272&bledit=1&dids=623"
-            className={styles.link}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Donate
-          </a>
+          <a className={styles.link} target="_blank" rel="noreferrer">Donate</a>
         </Link>
       </div>
     </div>
