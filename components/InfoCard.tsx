30aee79366c075b568cc132e3ab2b2d501921db7
JillianQuinn <jillian@jillianq.com>
2021-08-30 11:50:11 -0700

    pr changes

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -1,19 +1,31 @@
-import * as React from 'react';
+import React from 'react';
+import Image from 'next/image';
 import styles from '../styles/InfoCard.module.css';
 
 interface InfoCardProps {
-    Title: string;
-    Image: string;
-    Body: string;
+    title: string;
+    image: string;
+    body: string;
+    alt: string;
 }
 
 const InfoCard: React.FC<InfoCardProps> = (props: InfoCardProps): React.ReactElement => {
-  const { Title, Image, Body } = props;
+  const {
+    title, image, body, alt,
+  } = props;
   return (
     <div>
-      {Image && <img src={Image} alt="InfoCard" className={styles.ICImage} />}
-      <div className={styles.ICTitle}>{Title}</div>
-      <p className={styles.ICBody}>{Body}</p>
+      {image && (
+      <Image
+        src={image}
+        alt={alt}
+        width={380}
+        height={264}
+        className={styles.ICImage}
+      />
+      )}
+      <h4 className={styles.ICTitle}>{title}</h4>
+      <p className={styles.ICBody}>{body}</p>
     </div>
   );
 };
