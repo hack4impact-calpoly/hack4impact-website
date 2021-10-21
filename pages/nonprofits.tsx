e803b6a265148c70872392c9c258a5ebf252b16e
Ethan Zimbelman <itszimbo@gmail.com>
2021-10-21 12:30:46 -0700

    add FAQ info to student and nonprofit pages

--- pages/nonprofits.tsx
+++ pages/nonprofits.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import Head from 'next/head';
+import contentful from '../utils/contentful';
+import { FAQItem } from '../utils/types';
 
-const Nonprofits = () => (
-  <>
-    <Head>
-      <title>Nonprofits - Hack4Impact Cal Poly</title>
-    </Head>
-    <main>
-      <h1>Nonprofit Organizations</h1>
-      <p>for the wonderful folks that make the community :)</p>
-    </main>
-  </>
-);
+import FAQ from '../components/FAQ';
+
+interface NonprofitsProps {
+  faq: FAQItem[]
+}
+
+const Nonprofits = (props: NonprofitsProps) => {
+  const { faq } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Nonprofits - Hack4Impact Cal Poly</title>
+      </Head>
+      <main>
+        <h1>Nonprofit Organizations</h1>
+        <p>for the wonderful folks that make the community :)</p>
+
+        <FAQ questions={faq} />
+      </main>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const pageQuery = `{
+    faq: faqCollection(where:{page:"nonprofits"}) {
+      items {
+        question
+        answer
+      }
+    }
+  }`;
+
+  const res = await contentful.query(pageQuery);
+  const faq = res.faq.items as FAQItem[];
+
+  return {
+    props: {
+      faq,
+    },
+  };
+}
 
 export default Nonprofits;
