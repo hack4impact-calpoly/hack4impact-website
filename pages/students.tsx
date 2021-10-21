6af8e9a36599fbaf0dd5c6983cb558f06ae51c7e
Ethan Zimbelman <itszimbo@gmail.com>
2021-10-21 12:33:44 -0700

    FAQs added to site from Contentful (#54)
    * add heroicons package
    
    * created FAQ component and types for contentful
    
    * add FAQ info to student and nonprofit pages

--- pages/students.tsx
+++ pages/students.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import Head from 'next/head';
+import contentful from '../utils/contentful';
+import { FAQItem } from '../utils/types';
 
-const Students = () => (
-  <>
-    <Head>
-      <title>Students - Hack4Impact Cal Poly</title>
-    </Head>
-    <main>
-      <h1>Students</h1>
-      <p>for the folks who make up the club</p>
-    </main>
-  </>
-);
+import FAQ from '../components/FAQ';
+
+interface StudentsProps {
+  faq: FAQItem[]
+}
+
+const Students = (props: StudentsProps) => {
+  const { faq } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Students - Hack4Impact Cal Poly</title>
+      </Head>
+      <main>
+        <h1>Students</h1>
+        <p>for the folks who make up the club</p>
+
+        <FAQ questions={faq} />
+      </main>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const pageQuery = `{
+    faq: faqCollection(where:{page:"students"}) {
+      items {
+        question
+        answer
+      }
+    }
+  }`;
+
+  const res = await contentful.query(pageQuery);
+  const faq = res.faq.items as FAQItem[];
+
+  return {
+    props: {
+      faq,
+    },
+  };
+}
 
 export default Students;
