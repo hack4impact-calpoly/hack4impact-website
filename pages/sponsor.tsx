6c91cfbd6735354aa145318f4dfaffc5da19f910
Ethan Zimbelman <itszimbo@gmail.com>
2021-10-20 22:14:36 -0700

    init placeholder pages

---
+++ pages/sponsor.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Sponsor = () => (
+  <>
+    <Head>
+      <title>Sponsor - Hack4Impact Cal Poly</title>
+    </Head>
+    <main>
+      <h1>Sponsorship</h1>
+      <p>the people and companies making this financially feasible</p>
+    </main>
+  </>
+);
+
+export default Sponsor;
