c2869f7a53e2214b9183e565f2e788acb0bca2a6
Ethan Zimbelman <itszimbo@gmail.com>
2021-10-22 01:24:47 -0700

    tweak: remove FAQ header, add cursor pointer

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -10,8 +10,8 @@ const Question = (props: FAQItem) => {
     <div className="space-y-6 my-6">
       <div className="flex justify-between items-center">
         <h4>{question}</h4>
-        {!expanded && <ChevronDownIcon className="h-7 w-7 text-blue" onClick={() => toggleExpand(!expanded)} />}
-        {expanded && <ChevronUpIcon className="h-7 w-7 text-blue" onClick={() => toggleExpand(!expanded)} />}
+        {!expanded && <ChevronDownIcon className="h-7 w-7 text-blue cursor-pointer" onClick={() => toggleExpand(!expanded)} />}
+        {expanded && <ChevronUpIcon className="h-7 w-7 text-blue cursor-pointer" onClick={() => toggleExpand(!expanded)} />}
       </div>
       {expanded && <p>{answer}</p>}
       <hr />
@@ -23,11 +23,10 @@ const FAQ = (props: FAQItems) => {
   const { questions } = props;
 
   return (
-    <div>
-      <h2>Frequently Asked Questions</h2>
+    <>
       {/* eslint-disable-next-line react/no-array-index-key */}
       {questions.map((q, i) => <Question key={i} question={q.question} answer={q.answer} />)}
-    </div>
+    </>
   );
 };
 
