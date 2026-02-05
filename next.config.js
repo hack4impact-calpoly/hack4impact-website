91fcf3955007d238333fb6934d6d3d639a430299
Jesus Avalos <jesusavalosr2004@gmail.com>
2026-02-05 15:23:40 -0800

    fix: export-issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-   output: 'export',
-  reactStrictMode: true,
-  images: {
-    domains: ['images.ctfassets.net', 'calpoly.hack4impact.org'],
-    loader: 'imgix',
-    path: 'https://calpoly.hack4impact.org/',
-  },
+    output: "export",
+    reactStrictMode: true,
+    images: {
+        domains: ["images.ctfassets.net", "calpoly.hack4impact.org"],
+        loader: "imgix",
+        path: "https://calpoly.hack4impact.org/",
+    },
 };
